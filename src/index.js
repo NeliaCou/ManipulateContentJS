b753fd321d7c9ab63c1a53e9062e2637f2860089
Nelia <137152184+NeliaCou@users.noreply.github.com>
2023-09-25 11:08:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const title = document.querySelector(".title");
+const box = document.querySelector(".box");
+box.style.backgroundColor = "yellow";
+box.style.width = "400px";
+box.style.height = "300px";
+title.style.fontSize = "60px";
+title.style.color = "lightblue";
+title.style.textAlign = "center";
